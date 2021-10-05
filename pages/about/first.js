5e7645c4c53d79a14f282cfaec10964d755f4cb2
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2021-10-05 18:52:42 +0600

    first commit

---
+++ pages/about/first.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const first = () => {
+    return (
+        <div>
+            <h1>This is first nested route by about page</h1>
+            
+        </div>
+    );
+};
+
+export default first;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <h1>Welcome to the nextJs boilerplate</h1>
+                      
+        </div>
+    );
+};
+
+export default index;
